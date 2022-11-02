fcc73fb92336b494a7825d4ab675acc5955dfeaf
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-02 12:17:29 -0500

    Create 'Add 'get_words' method

--- decoder_morse.rb
+++ decoder_morse.rb
@@ -4,10 +4,21 @@
   "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",
   ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
   "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
-  "--.." => "Z"
+  "--.." => "Z", ".----" => "1", "..---" => "2", "...--" => "3",
+  "....-" => "4", "....." => "5", "-...." => "6", "--..." => "7",
+  "---.." => "8", "----." => "9", "-----" => "0", ".-.-.-" => ".",
 }
 
 def get_letters(character)
   @current_letter = @morse_alphabet[character]
   @current_letter
+end
+
+def get_words(word)
+  @current_word = ''
+  word.each do |character|
+    get_letters(character)
+    @current_word += get_letters(character)
+  end
+  @current_word
 end
